a4652768a5e0304d681453c0b8ded5bff6199759
irmawang1025 <irmawang1025@gmail.com>
2025-08-19 07:52:42 +0000

    1

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 def index(): 
     return(render_template("index.html"))
 
-@app.route("/",methods=["GET","POST"])
+@app.route("/main",methods=["GET","POST"])
 def main(): 
     #database
     return(render_template("main.html"))
